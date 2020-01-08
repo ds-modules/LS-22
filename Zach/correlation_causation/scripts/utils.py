01f799a3734f84a4bd342f1d2bb3bd9109047288
ktakimoto <ktakimoto@berkeley.edu>
2020-01-08 13:32:20 -0800

    finish part 1 text + code

--- Zach/correlation_causation/scripts/utils.py
+++ Zach/correlation_causation/scripts/utils.py
@@ -1,6 +1,11 @@
 """Full urls for data sets loaded into correlation-causation notebook.
 Hidden in a separate file to improve notebook cleanliness and avoid data set spoilers."""
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+from scipy.stats import pearsonr
+
 
 # USDA agriculture food consumption data
 # from Tyler Veigen's Spurious Correlations
@@ -24,3 +29,19 @@ mystery_var2 = raw_engr_data.iloc[24, 2:].astype('float')
 # quick and dirty cleaning
 engr_data = raw_engr_data.dropna().set_index("Field").drop(1999, axis=1)
 
+# function for the datasaurus widget
+def analyze_data(dataset_num):
+    data = pd.read_csv("https://raw.githubusercontent.com/lockedata/datasauRus/master/inst/extdata/DatasaurusDozen.tsv", 
+            index_col="dataset", sep='\t')
+    dataset_list = data.index[::-1].unique()
+    data = data.loc[dataset_list[dataset_num -1], :]
+    fig = plt.figure(figsize=(14, 6));
+    ax1=fig.add_subplot(121);
+    sns.scatterplot(data=data, x="x", y="y");
+    ax2 = fig.add_subplot(122);
+    ax2.axis('off');
+    s = pd.Series([data.x.mean(),np.std(data.x),data.y.mean(),np.std(data.y), 
+                          pearsonr(data.x, data.y)[0],pearsonr(data.x, data.y)[1]]).apply(str)
+    cell_vals = list(zip(['mean x', 'SD x', 'mean y', 'SD y', 'correlation', 'p-value'],s))
+    ax2.table(cellText = cell_vals, bbox=[0, 0, 1, 1]);
+    plt.show();
